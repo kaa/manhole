887c8a231c86632df28c5256d38e38483ebeca7a
kaa <jbeijar@mios.fi>
2013-07-26 15:22:39 +0300

    Bump package version.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 "\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" Manhole.sln /p:Configuration=Release
 @cmd /c .nuget\nuget pack Manhole\Manhole.nuspec
-@cmd /c .nuget\nuget pack Manhole.Web\Manhole.Web.nuspec	
+@cmd /c .nuget\nuget pack Manhole.Web\Manhole.Web.nuspec
